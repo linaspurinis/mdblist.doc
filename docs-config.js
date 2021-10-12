4050a0b346b25e6cf96ef4aa79873733db0493d7
Linas <linas.purinis@gmail.com>
2021-10-12 15:55:29 +0300

    Create docs-config.js

---
+++ docs-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  product: "Docs Engine",
+  pathPrefix: "/docs-engine",
+  productIconKey: "docs-engine",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/docs-engine",
+  externalLinks: [
+    {
+      title: "Docs Engine on GitHub",
+      url: "https://github.com/cloudflare/cloudflare-docs-engine"
+    },
+    {
+      title: "Cloudflare Developer documentation",
+      url: "https://developers.cloudflare.com/docs"
+    },
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+  },
+  siteMetadata: {
+    title: "Cloudflare Docs Engine docs",
+    description: "Documentation for the open-source Cloudflare Documentation engine which powers Cloudflare's open-source documentation.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/docs-engine",
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png",
+  }
+}
